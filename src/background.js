5dbdfbccfef85fa9a7c8b3be0a29db16ec657430
Nick <50417170+kanishev@users.noreply.github.com>
2022-02-18 22:02:50 +0500

    add pinner image-text post data functions

--- src/background.js
+++ src/background.js
@@ -14,24 +14,17 @@ chrome.storage.local.get().then(function (storage) {
 
 chrome.contextMenus.onClicked.addListener(function (target) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { target, token }, function () {
-      console.log("sent target");
-    });
+    chrome.tabs.sendMessage(tabs[0].id, { target, token });
   });
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.pinnerStatus) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        { pinnerStatus: request.pinnerStatus },
-        function () {
-          console.log("sent message");
-        }
-      );
+      chrome.tabs.sendMessage(tabs[0].id, {
+        pinnerStatus: request.pinnerStatus,
+      });
     });
-    sendResponse("status has been received");
   } else if (request == "getToken") {
     sendResponse(token);
   }
@@ -52,9 +45,7 @@ chrome.runtime.onMessageExternal.addListener(function (
       chrome.action.enable();
       chrome.contextMenus.create(contextMenu);
       token = request.token;
-      chrome.storage.local.set({ token }, function () {
-        console.log("Value is set to " + token);
-      });
+      chrome.storage.local.set({ token });
       sendResponse({ message: "Token has been recievedd" });
     }
   }
--- src/content.js
+++ src/content.js
@@ -1,6 +1,8 @@
 // let proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 let pinner = null;
+let alert = null;
+let selectedElement = null;
 
 chrome.runtime.onMessage.addListener(function (message) {
   if (message.pinnerStatus) {
@@ -8,14 +10,14 @@ chrome.runtime.onMessage.addListener(function (message) {
   }
 
   if (message.target && message.target.mediaType == "image") {
-    getBase64Image(message.target.srcUrl, (urls) => {
+    getBase64Image(message.target.srcUrl, (path) => {
       const postData = {
         data: [],
       };
-      const item = {};
       postData.token = message.token;
+      const item = {};
       item.type = "image";
-      item.base64 = urls;
+      item.base64 = path;
       item.url = message.target.srcUrl;
 
       postData.data.push(item);
@@ -25,8 +27,6 @@ chrome.runtime.onMessage.addListener(function (message) {
     const postData = {
       data: [],
     };
-
-    console.log(selectedElement);
     const item = {};
     postData.token = message.token;
     item.type = "html";
@@ -54,9 +54,13 @@ function getBase64Image(url, callback) {
 
 function setPostData(content) {
   console.log(content);
+  alert.style.display = "flex";
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 3000);
 }
 
-document.onselectionchange = function (e) {
+document.onselectionchange = function () {
   const selection = document.getSelection();
 
   if (!selection.isCollapsed) {
@@ -73,17 +77,29 @@ document.onselectionchange = function (e) {
   }
 
   if (selection.anchorNode.id == "wejePinner") {
-    createPostData();
+    createPostData(selectedElement);
   }
+  setPinnerCoords(selection);
+};
 
+// pinner
+
+pinner = document.createElement("div");
+pinner.classList.add("my-class");
+pinner.id = "wejePinner";
+pinner.style =
+  "position: absolute;top: 10px;left: 10px;width: 20px;height: 20px;background: blue;border-radius:50%; z-index: 10000;cursor: pointer; display:none";
+document.body.append(pinner);
+
+function setPinnerCoords(selection) {
   if (selection.isCollapsed) {
     pinner.style.top = 0;
     pinner.style.left = 0;
   } else {
     pinner.style.top = selection.anchorNode.parentNode.offsetTop - 15 + "px";
-    pinner.style.left = selection.anchorNode.parentNode.offsetLeft - 15 + "px";
+    pinner.style.left = selection.anchorNode.parentNode.offsetLeft - 20 + "px";
   }
-};
+}
 
 function togglePinner({ status, type }) {
   if (status == "on") {
@@ -93,26 +109,52 @@ function togglePinner({ status, type }) {
   }
 }
 
-pinner = document.createElement("div");
-pinner.classList.add("my-class");
-pinner.id = "wejePinner";
-pinner.style =
-  "position: absolute;top: 10px;left: 10px;width: 10px;height: 10px;background: brown;z-index: 10000;cursor: pointer; display:none";
+document.onmousemove = function (e) {
+  if (e.target.nodeName == "IMG") {
+    pinner.style.top = e.target.offsetTop + 10 + "px";
+    pinner.style.left = e.target.offsetLeft + 10 + "px";
+    selectedElement = e.target;
+  }
+};
 
-document.body.append(pinner);
+// alert
 
-function createPostData() {
+alert = document.createElement("div");
+alert.classList.add("my-alert");
+alert.id = "wejeAlert";
+alert.style =
+  "display:none;z-index: 100;width: 200px;height: 50px;background: deepskyblue;position: fixed;top: 0;left: 50%;border-bottom-right-radius: 10px;border-bottom-left-radius: 10px; justify-content: center;align-items: center;color: white;";
+alert.innerText = "SENT CONTENT";
+document.body.append(alert);
+
+function createPostData(node) {
   chrome.runtime.sendMessage("getToken", function (token) {
-    const postData = {
-      data: [],
-    };
-    const item = {};
-    postData.token = token;
-    item.type = "html";
-    item.html = selectedElement.outerHTML;
-    item.text = selectedElement.innerText;
+    if (node.nodeName == "IMG") {
+      getBase64Image(selectedElement.src, (path) => {
+        const postData = {
+          data: [],
+        };
+        postData.token = token;
+        const item = {};
+        item.type = "image";
+        item.base64 = path;
+        item.url = selectedElement.src;
 
-    postData.data.push(item);
-    setPostData(postData);
+        postData.data.push(item);
+        setPostData(postData);
+      });
+    } else {
+      const postData = {
+        data: [],
+      };
+      postData.token = token;
+      const item = {};
+      item.type = "image";
+      item.html = selectedElement.outerHTML;
+      item.text = selectedElement.innerText;
+
+      postData.data.push(item);
+      setPostData(postData);
+    }
   });
 }
